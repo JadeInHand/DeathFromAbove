e23cc830f0206afeae09123405dd13d6ccd37e86
JadeInHand <cpurcell22@gmail.com>
2015-10-21 11:21:52 +1100

    first commit, all player, enemy functionality complete some boss functionality complete

---
+++ app/assets/javascripts/all.js
@@ -0,0 +1,6 @@
+//= require _phaser-arcade-physics.min
+//= require _boot
+//= require _preloader
+//= require _mainMenu
+//= require _app
+//= require _game
---
+++ db/migrate/20151020010734_highscores.rb
@@ -0,0 +1,4 @@
+class Highscores < ActiveRecord::Migration
+  def change
+  end
+end
